a99272eceaf278e10e4409a797bf0593fc938d53
koudle <rxs086@gmail.com>
2018-12-22 01:16:29 +0800

    新增macOS工程

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gdg_weather/page/city/CityWidget.dart';
 import 'package:gdg_weather/page/weather/WeatherWidget.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  _setTargetPlatformForDesktop();
+  runApp(MyApp());
+}
+
+/// If the current platform is desktop, override the default platform to
+/// a supported platform (iOS for macOS, Android for Linux and Windows).
+/// Otherwise, do nothing.
+void _setTargetPlatformForDesktop() {
+  TargetPlatform targetPlatform;
+  if (Platform.isMacOS) {
+    targetPlatform = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    targetPlatform = TargetPlatform.android;
+  }
+  if (targetPlatform != null) {
+    debugDefaultTargetPlatformOverride = targetPlatform;
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
